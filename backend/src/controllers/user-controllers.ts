73fc4cf9cf067944d6dad07ab4a69e725f676d3d
Omielee <omielee@hotmail.com>
2024-01-26 12:04:36 +0000

    authentication

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,65 +1,96 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/User.js";
-import {hash,compare} from 'bcrypt';
+import { hash, compare } from 'bcrypt';
+import { TokenExpiredError } from "jsonwebtoken";
+import { createToken } from "../utils/token_managers.js";
+import { COOKIE_NAME } from "../utils/constants.js";
 
-export const getALLUsers = async(
-    req:Request, 
-    res:Response,
+export const getALLUsers = async (
+    req: Request,
+    res: Response,
     next: NextFunction
 ) => {
     try {
         // get all users directly from database
         const users = await User.find();
-        return res.status(200).json({message:"ok",users});
+        return res.status(200).json({ message: "ok", users });
     } catch (error) {
         console.log(error);
-        return res.status(500).json({message:"error",cause: error.message});
+        return res.status(500).json({ message: "error", cause: error.message });
     }
-    
+
 }
 
-export const userSignup = async(
-    req:Request, 
-    res:Response,
+export const userSignup = async (
+    req: Request,
+    res: Response,
     next: NextFunction
 ) => {
     try {
         // user signup
-        const {name, email, password} = req.body;
-        const existingUser = await User.findOne({email});
+        const { name, email, password } = req.body;
+        const existingUser = await User.findOne({ email });
         if (existingUser) return res.status(401).send("User already registerd");
-        const hashedPassword = await hash(password,10);
-        const user = new User({name,email,password:hashedPassword});
+        const hashedPassword = await hash(password, 10);
+        const user = new User({ name, email, password: hashedPassword });
         await user.save();
-        return res.status(201).json({message:"sign up ok",id:user._id.toString()});
+
+        //create token and store cookie
+        res.clearCookie(COOKIE_NAME,{
+            httpOnly:true,
+            domain:"localhost",
+            signed:true,
+            path:"/",
+        });
+
+        const token = createToken(user.id.toString(), user.email, "7d");
+        const expires = new Date();
+        expires.setDate(expires.getDate() + 7);
+        res.cookie(COOKIE_NAME, token, { path: "/", domain: "localhost", expires, httpOnly: true, signed: true, });
+
+        return res.status(201).json({ message: "sign up ok", id: user._id.toString() });
     } catch (error) {
         console.log(error);
-        return res.status(500).json({message:"error",cause: error.message});
+        return res.status(500).json({ message: "error", cause: error.message });
     }
-    
+
 }
 
-export const userLogin = async(
-    req:Request, 
-    res:Response,
+export const userLogin = async (
+    req: Request,
+    res: Response,
     next: NextFunction
 ) => {
     try {
         // user login
-        const {email, password} = req.body;
-        const user = await User.findOne({email});
-        if(!user) {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
             return res.status(401).send("User not registered");
         }
-        const isPasswordCorrect = await compare(password,user.password);
+        const isPasswordCorrect = await compare(password, user.password);
         if (!isPasswordCorrect) {
             return res.status(403).send("Incorrect Password");
         }
 
-        return res.status(201).json({message:"login successfully",id:user._id.toString()});
+        res.clearCookie(COOKIE_NAME,{
+            httpOnly:true,
+            domain:"localhost",
+            signed:true,
+            path:"/",
+        });
+
+
+
+        const token = createToken(user.id.toString(), user.email, "7d");
+        const expires = new Date();
+        expires.setDate(expires.getDate() + 7);
+        res.cookie(COOKIE_NAME, token, { path: "/", domain: "localhost", expires, httpOnly: true, signed: true, });
+
+        return res.status(201).json({ message: "login successfully", id: user._id.toString() });
     } catch (error) {
         console.log(error);
-        return res.status(500).json({message:"error",cause: error.message});
+        return res.status(500).json({ message: "error", cause: error.message });
     }
-    
+
 }
--- terminal.md
+++ terminal.md
@@ -27,3 +27,16 @@ user:
 
 6. Middleware
 Middleware are functions which gets exected before a request is processed. In Node and Express, middleware can be used to check JSON Body Validations, Tokens or Cookies Validations, Params Validations and more according to requirements.
+
+7. Authentication
+Authentication is a step inwhich the user needs to verify their identity. For this application, the user needs to provide the Email and Password which it created during the registration process. The user will be provided a Token After Auth Process.
+
+8. Authorization
+Once the user authenticates, he is provided a token. now to access a resource, the user needs to show a token that was sent during authentication. This ensures that the user is entitled to a resource.
+
+9. JWT
+JSON Web Token(JWT) is used to encrypt a payload into a signed token that has the permissions or authorities of the user.
+
+10. HTTP only cookies
+http-only cookies are a type of web cookie that comes with a special security attribute that restricts cookies from being accessed by Javascript in the web-browser. This prevents XSS attacks.
+
